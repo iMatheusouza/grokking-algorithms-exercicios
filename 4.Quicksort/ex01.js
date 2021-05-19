3aa2737b232f9146a50042ff1265cfe3891a397d
Matheus <matheus.freitas@sonatalab.com>
2021-05-19 10:19:32 -0300

    first commit

---
+++ 4.Quicksort/ex01.js
@@ -0,0 +1,12 @@
+function sum(arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+
+  else {
+    let number = arr.pop();
+    return number + sum(arr);
+  }
+}
+
+console.log(sum([2, 4, 6]));//resultado esperado 12
